990870283b74ba1267571848e7b344655f1a7b7c
JimLewis <jim@synthworks.com>
2016-12-12 13:25:35 -0800

    Fixed the attribute for VendorCovBinAdd.   The OSVVM release referred to an older name.

--- VendorCovApiPkg_Aldec.vhd
+++ VendorCovApiPkg_Aldec.vhd
@@ -3,17 +3,18 @@
 --  Design Unit Name:  VendorCovApiPkg
 --  Revision:          ALDEC VERSION
 --
---  Maintainer:        
+--  Maintainer:
 --
 --  Package Defines
 --     A set of foreign procedures that link OSVVM's CoveragePkg
---     coverage model creation and coverage capture with the 
+--     coverage model creation and coverage capture with the
 --     built-in capability of a simulator.
 --
 --
 --  Revision History:      For more details, see CoveragePkg_release_notes.pdf
 --    Date      Version    Description
 --    11/2016:  2016.11    Initial revision
+--    12/2016   2016.11a   Fixed an issue with attributes
 --
 --
 --  Copyright (c) 2016 by Aldec.  All rights reserved.
@@ -21,8 +22,8 @@
 --  Verbatim copies of this source file may be used and
 --  distributed without restriction.
 --
---  Modified copies of this source file may be distributed 
---  under the terms of the ARTISTIC License as published by 
+--  Modified copies of this source file may be distributed
+--  under the terms of the ARTISTIC License as published by
 --  The Perl Foundation; either version 2.0 of the License,
 --  or (at your option) any later version.
 --
@@ -63,17 +64,17 @@ package VendorCovApiPkg is
   --  Should not be called until the data structure is created by VendorCovPointCreate or VendorCovCrossCreate.
   --  Replaces name that was set by VendorCovPointCreate or VendorCovCrossCreate.
   procedure VendorCovSetName( obj: VendorCovHandleType; name: string );
-  attribute foreign of VendorCovSetName[ VendorCovHandleType, string ]: procedure is "VHPI systf; cvg_SetCoverName";	
+  attribute foreign of VendorCovSetName[ VendorCovHandleType, string ]: procedure is "VHPI systf; cvg_SetCoverName";
 
   --  Add a bin or set of bins to either a Point/Item or Cross Functional Coverage Model
   --  Checking for sizing that is different from original sizing already done in OSVVM CoveragePkg
-  --  It is important to maintain an index that corresponds to the order the bins were entered as 
+  --  It is important to maintain an index that corresponds to the order the bins were entered as
   --  that is used when coverage is recorded.
   procedure VendorCovBinAdd( obj: VendorCovHandleType; bins: VendorCovRangeArrayType; Action: integer; atleast: integer; name: string );
-  attribute foreign of CvpCrBinCreate[ VendorCovHandleType, VendorCovRangeArrayType, integer, integer, string ]: procedure is "VHPI systf; cvg_CvpCrBinCreate";
+  attribute foreign of VendorCovBinAdd[ VendorCovHandleType, VendorCovRangeArrayType, integer, integer, string ]: procedure is "VHPI systf; cvg_CvpCrBinCreate";
 
   --  Increment the coverage of bin identified by index number.
-  --  Index ranges from 1 to Number of Bins.  
+  --  Index ranges from 1 to Number of Bins.
   --  Index corresponds to the order the bins were entered (starting from 1)
   procedure VendorCovBinInc( obj: VendorCovHandleType; index: integer );
   attribute foreign of VendorCovBinInc[ VendorCovHandleType, integer ]: procedure is "VHPI systf; cvg_CvpCrBinIncr";
@@ -81,7 +82,7 @@ package VendorCovApiPkg is
   -- Action (integer):
   -- constant COV_COUNT   : integer := 1;
   -- constant COV_IGNORE  : integer := 0;
--- constant COV_ILLEGAL : integer := -1;
+  -- constant COV_ILLEGAL : integer := -1;
 
 end package;
 
