acd3c3d00980d6683162eb200cfe21bd8e3645f2
Jim Lewis <jim@synthworks.com>
2022-04-10 17:58:24 -0700

    Added package reference for to_hxstring call

--- ScoreboardPkg_slv.vhd
+++ ScoreboardPkg_slv.vhd
@@ -51,6 +51,7 @@ library ieee ;
   use ieee.std_logic_1164.all ;
   use ieee.numeric_std.all ;
  
+use work.TextUtilPkg.all ; 
 
 package ScoreBoardPkg_slv is new work.ScoreboardGenericPkg
   generic map (
