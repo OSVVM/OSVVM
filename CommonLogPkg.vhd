9feaa3da7b99740cfa55f986c64420a3d8d50bd3
Lars Asplund <lars.anders.asplund@gmail.com>
2023-07-16 23:02:49 +0200

    Removed mandatory no-name parameters from common log interface.

--- CommonLogPkg.vhd
+++ CommonLogPkg.vhd
@@ -7,10 +7,6 @@ package CommonLogPkg is
 
   -- Converts a log message and associated metadata to a string written to the specified log destination.
   procedure WriteToLog(
-    ----------------------------------------------------
-    -- Log entry items common to many logging frameworks
-    ----------------------------------------------------
-
     -- Destination of the log message is either std.textio.output (std output) or a text file object previously opened
     -- for writing
     file LogDestination : text;
@@ -24,15 +20,6 @@ package CommonLogPkg is
     LogLevel : string := NoString;
     -- Name of the producer of the log message. Hierarchical names use colon as the delimiter.
     -- For example "parent_component:child_component".
-    LogSourceName : string := NoString;
-
-    ----------------------------------------------------------------------------------------------------------------------------
-    -- Log entry items less commonly used are passed to the procedure with no-name string, integer and boolean parameters which
-    -- meaning is specific to an implementation of the procedure. These are not used by OSVVM.
-    ----------------------------------------------------------------------------------------------------------------------------
-
-    Str1, Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9, Str10 : string := "";
-    Int1, Int2, Int3, Int4, Int5, Int6, Int7, Int8, Int9, Int10 : integer := 0;
-    Bool1, Bool2, Bool3, Bool4, Bool5, Bool6, Bool7, Bool8, Bool9, Bool10 : boolean := false
+    LogSourceName : string := NoString
   );
 end package;
--- CommonLogPkgBody.vhd
+++ CommonLogPkgBody.vhd
@@ -7,11 +7,7 @@ package body CommonLogPkg is
     Msg : string := NoString;
     LogTime : time := NoTime;
     LogLevel : string := NoString;
-    LogSourceName : string := NoString;
-
-    Str1, Str2, Str3, Str4, Str5, Str6, Str7, Str8, Str9, Str10 : string := "";
-    Int1, Int2, Int3, Int4, Int5, Int6, Int7, Int8, Int9, Int10 : integer := 0;
-    Bool1, Bool2, Bool3, Bool4, Bool5, Bool6, Bool7, Bool8, Bool9, Bool10 : boolean := false
+    LogSourceName : string := NoString
   ) is
   begin
   end;
