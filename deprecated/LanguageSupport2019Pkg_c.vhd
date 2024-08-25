761142a47082ff400533e415f0d43267b36c765b
Jim Lewis <jim@synthworks.com>
2024-08-24 17:03:22 -0700

    Spacing

--- deprecated/LanguageSupport2019Pkg_c.vhd
+++ deprecated/LanguageSupport2019Pkg_c.vhd
@@ -10,7 +10,7 @@
 --
 --  Description:
 --     Utilities to stand in for things added in 1076-2019
---          
+--
 --
 --  Developed for:
 --        SynthWorks Design Inc.
@@ -24,26 +24,26 @@
 --
 --
 --  This file is part of OSVVM.
---  
---  Copyright (c) 2024 by SynthWorks Design Inc.  
---  
+--
+--  Copyright (c) 2024 by SynthWorks Design Inc.
+--
 --  Licensed under the Apache License, Version 2.0 (the "License");
 --  you may not use this file except in compliance with the License.
 --  You may obtain a copy of the License at
---  
+--
 --      https://www.apache.org/licenses/LICENSE-2.0
---  
+--
 --  Unless required by applicable law or agreed to in writing, software
 --  distributed under the License is distributed on an "AS IS" BASIS,
 --  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
---  
+--
 
 use std.textio.all ;
-library ieee ; 
-use ieee.std_logic_1164.all ; 
-use ieee.numeric_std.all ; 
+library ieee ;
+use ieee.std_logic_1164.all ;
+use ieee.numeric_std.all ;
 
 package LanguageSupport2019Pkg is
 
@@ -54,38 +54,38 @@ package LanguageSupport2019Pkg is
   impure function to_string ( A : integer_vector) return string ;
 
 end LanguageSupport2019Pkg ;
-  
+
 --- ///////////////////////////////////////////////////////////////////////////
 --- ///////////////////////////////////////////////////////////////////////////
 --- ///////////////////////////////////////////////////////////////////////////
 
 package body LanguageSupport2019Pkg is
   ------------------------------------------------------------
-  -- to_string  
-  impure function to_string ( A : integer_vector) return string is 
+  -- to_string
+  impure function to_string ( A : integer_vector) return string is
   ------------------------------------------------------------
-    alias normA : integer_vector(1 to A'length) is A ; 
-    
-    variable buf : line ; 
+    alias normA : integer_vector(1 to A'length) is A ;
+
+    variable buf : line ;
     -- with VHDL-2019 this could be general purpose with buf as a parameter
-    impure function local_to_string return string is 
-      variable result : string(1 to buf'length) ; 
-    begin 
-      result := buf.all ; 
-      deallocate(buf) ; 
-      return result ; 
-    end function local_to_string ; 
-      
+    impure function local_to_string return string is
+      variable result : string(1 to buf'length) ;
+    begin
+      result := buf.all ;
+      deallocate(buf) ;
+      return result ;
+    end function local_to_string ;
+
   begin
-    swrite(buf, "(") ; 
+    swrite(buf, "(") ;
     for i in 1 to A'length-1 loop
       write(buf, A(i)) ;
       swrite(buf, ",") ;
-    end loop ; 
+    end loop ;
     write(buf, A(A'length)) ;
-    swrite(buf, ")") ; 
+    swrite(buf, ")") ;
 
-    return local_to_string ; 
+    return local_to_string ;
   end function to_string ;
 
 end package body LanguageSupport2019Pkg ;
