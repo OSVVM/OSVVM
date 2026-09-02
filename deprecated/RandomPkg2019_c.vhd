5ce0800b122e5dc5499deefc2d3e1235150d2728
Jim Lewis <jim@synthworks.com>
2026-09-02 07:54:48 -0700

    Added constant SUPPORTS_2019_IMPURE_FUNCTIONS

--- deprecated/RandomPkg2019_c.vhd
+++ deprecated/RandomPkg2019_c.vhd
@@ -21,26 +21,27 @@
 --
 --  Revision History :
 --    Date       Version    Description
---    09/2024    2024.09    Derived from RandomPkg 
+--    09/2024    2024.09    Derived from RandomPkg
 --
 --  This file is part of OSVVM.
---  
---  Copyright (c) 2024 by SynthWorks Design Inc.  
---  Copyright (C) 2024 by OSVVM Authors   
---  
+--
+--  Copyright (c) 2024 by SynthWorks Design Inc.
+--  Copyright (C) 2024 by OSVVM Authors
+--
 --  Licensed under the Apache License, Version 2.0 (the "License");
 --  you may not use this file except in compliance with the License.
 --  You may obtain a copy of the License at
---  
+--
 --      https://www.apache.org/licenses/LICENSE-2.0
---  
+--
 --  Unless required by applicable law or agreed to in writing, software
 --  distributed under the License is distributed on an "AS IS" BASIS,
 --  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
---  
+--
 
 package RandomPkg2019 is
+  constant SUPPORTS_2019_IMPURE_FUNCTIONS : boolean := FALSE ;
 
 end RandomPkg2019 ;
